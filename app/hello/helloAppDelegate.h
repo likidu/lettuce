0ddd9dc6d140a69600e238b8a54c1a2d8eb5d39b
filebat <filebat.mark@gmail.com>
2013-04-06 20:14:32 +0800

    weibo authentication

--- app/hello/helloAppDelegate.h
+++ app/hello/helloAppDelegate.h
@@ -8,17 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
+// #################  Mixpannel #########################
+#define MIXPANEL_TOKEN @"03caa3c6e77bab5ef3ab8dfdd8ef3fac"
+// ######################################################
+
 // ############## weibo #################################
 // TODO denny
 #define kAppKey             @"1948323577"
 #define kAppSecret          @"a9a6ba85eca70e70c227914ba2405845"
 #define kAppRedirectURI     @"http://open.weibo.com"
+
+@class SinaWeibo;
+@class SNViewController;
 // ######################################################
 
 @interface helloAppDelegate : NSObject <UIApplicationDelegate> {
-
+    SinaWeibo *sinaweibo;
 }
 
+@property (readonly, nonatomic) SinaWeibo *sinaweibo;
 @property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (strong, nonatomic) SNViewController *viewController;
 
 @end
