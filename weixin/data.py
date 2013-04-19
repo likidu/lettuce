8e14a9f56efc1241ea4c033d9bbb15e11428420a
filebat <filebat.mark@gmail.com>
2013-04-19 23:47:24 +0800

    dictionary

--- weixin/data.py
+++ weixin/data.py
@@ -7,15 +7,17 @@
 ## Description :
 ## --
 ## Created : <2013-01-25 00:00:00>
-## Updated: Time-stamp: <2013-04-19 10:03:01>
+## Updated: Time-stamp: <2013-04-19 23:46:48>
 ##-------------------------------------------------------------------
 # import MySQLdb
 from pymmseg import mmseg
 
 mmseg.dict_load_defaults()
+mmseg.dict_load_words("woojuu.dic")
+# python -c "import data; data.word_split('37,超大杯星巴克焦糖玛奇朵')"
 def word_split(sentence):
-    text = '''放学前，老师把学生叫到办公室，拿出一片止痛片说：你把它吃下去吧。学生不解的问：我哪儿也不疼啊。老师回答：过会儿就疼了，我已经把你考试不及格的事告诉你爸爸了'''
-    algor = mmseg.Algorithm(text)
+    sentence = '''37,超大杯星巴克焦糖玛奇朵'''
+    algor = mmseg.Algorithm(sentence)
     for tok in algor:
         print '%s [%d..%d]' % (tok.text, tok.start, tok.end)
     return None
